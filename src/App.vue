ad386910edf55f3e7acf5752bf3193ffa1d66871
Alert175 <40724267+Alert175@users.noreply.github.com>
2019-04-12 17:39:13 +0300

    resposible

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,22 @@
 </template>
 
 <style>
+  body{
+    margin: 0 auto;
+    padding: 0;
+    background: rgba(245, 245, 245, 0.91);
+  }
   .grid {
     display: grid;
     justify-content: center;
     align-items: center;
+  }
+  #nav{
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 10vh;
+    text-align: center;
+    width: 50%;
+    margin-left: 25%;
+    text-decoration: none;
   }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,18 @@
 <template lang="html">
-  <div class="wrapper-main">
+  <div class="wrapper-main grid">
+    <transition name="saved" mode="in-out">
+      <button type="button" class="save" v-on:click="save">
+        <i class="fas fa-cloud fa-4x"></i>
+      </button>
+    </transition>
     <input type="text" class="addItem" v-model="item" v-on:keyup.enter="addListItem" placeholder="Какие задачи на сегодня?">
     <ul class="listTasks">
       <li v-for="(task, id) in tasks" class="listItem" v-bind:class="{compliteTask: task.complite}">
-        <span type="checkbox" id="_check" v-on:click="completeTask(id, item.complite)">
-          <i class="far fa-circle fa-1x" v-if="!item.complite"></i>
-          <i class="fas fa-check fa-1x" v-if="item.complite"></i>
+        <span type="checkbox" id="_check" v-on:click="completeTask(id, task.complite)">
+          <i class="far fa-circle fa-1x" v-if="!task.complite"></i>
+          <i class="fas fa-check fa-1x" v-if="task.complite"></i>
         </span>
-          <span class="taskDescription">{{task.description}}/{{task.complite}}</span>
+          <span class="taskDescription">{{task.description}}</span>
           <button type="button" class="deleteTask" v-on:click="deleteListItem(id)">
             <i class="fas fa-backspace fa-2x"></i>
           </button>
@@ -17,9 +22,6 @@
 </template>
 
 <script>
-// window.onload = function(){
-//   alert(tasks)
-// }
 export default {
   data(){
     return{
@@ -29,22 +31,8 @@ export default {
     }
   },
   methods:{
-    // проверяю на пустой input > записываю данные в массив tasks и в localStorage
     addListItem: function(){
-          this.item == '' ? alert('Друг, укажи задачу :)') : this.tasks.push({description : this.item, complite: false, index: this.tasks.length-1});
-          // if (localStorage["task"] == undefined) {
-          //   localStorage["task"] = this.item + '|' + false + '/'
-          // } else {
-          //   localStorage["task"] += this.item + '|' + false + '/'
-          // }
-          // console.log(this.tasks.length);
-
-          let serialStorage = this.tasks[this.tasks.length-1];
-
-          serialStorage = JSON.stringify(serialStorage);
-          console.dir(serialStorage);
-          localStorage[this.tasks.length-1] = serialStorage;
-          localStorage["counter"] = this.tasks.length;
+          this.item == '' ? alert('Друг, укажи задачу :)') : this.tasks.push({description : this.item, complite: false});
           this.item = '';
     },
     completeTask: function(id, complite){
@@ -52,44 +40,20 @@ export default {
     },
     deleteListItem: function(id){
         this.tasks.splice(id, 1);
-        localStorage.removeItem(id);
-        console.log(id);
-        // localStorage["counter"] = localStorage["counter"] - 1
-
-        localStorage.clear();
     },
-    // проверяю localStorage на наличие данных по ключу false, и записываю в массив tasks
+    //сохраняю актуальный массив tasks в локальное хранилище в формате JSON
+    save: function(){
+      localStorage.clear();
+      console.log(JSON.stringify(this.tasks));
+      localStorage["tasks"] = JSON.stringify(this.tasks);
+    },
+    //Получаю массив tasks в формате JSON если он есть
     onload: function(){
-      // let storage = localStorage["task"];
-      //
-      // // console.log(this.tasks);
-      // storage = storage.split('/');
-      // storage.pop()
-      // // console.log(storage);
-      //
-      // for (var i = 0; i < storage.length; i++) {
-      //   // console.log(storage[i]);
-      //   this.tasks.push({description : storage[i], complite: false});
-      // }
-
-      let counter = localStorage["counter"];
-      console.log(counter);
-      if (counter == undefined) {
-        console.log('Выводить нечего');
+      if (localStorage["tasks"] !== undefined) {
+        this.tasks = JSON.parse(localStorage["tasks"]);
       }
       else {
-        for (var i = 0; i < counter; i++) {
-          console.log(i);
-
-          if (localStorage[i] == undefined) {
-            console.dir(localStorage[i] + ' localStorage[i]');
-          }
-          else {
-            this.tasks.push(JSON.parse(localStorage[i]));
-          }
-
-        }
-        console.dir(this.tasks);
+        console.log('локальное хранилище пусто');
       }
     }
   },
@@ -101,4 +65,98 @@ export default {
 </script>
 
 <style lang="css" scoped>
+
+  .wrapper-main{
+      display: grid;
+      grid-template-columns: 1fr;
+      grid-template-rows: 150px 80px auto;
+      grid-template-areas:
+          "heat"
+          "addItem"
+          "listTasks";
+      width: 50%;
+      margin-left: 25%;
+  }
+  .save{
+    height: 100%;
+    border: none;
+    background: rgba(245, 245, 245, 0.91);
+    -webkit-transition: .5s;
+    transition: .5s;
+  }
+  .save:focus{
+    -webkit-transform: rotateY(180deg);
+            transform: rotateY(180deg);
+  }
+  .head{
+      grid-area: heat;
+      text-align: center;
+      vertical-align: bottom;
+      font-size: 50px;
+      font-family: 'Josefin Slab', serif;
+      border-bottom: 1px solid rgba(47, 47, 47, 0.81);
+
+  }
+  .addItem{
+      grid-area: addItem;
+      background: rgba(245, 245, 245, 0.91);
+      border: 1px solid rgba(245, 245, 245, 0.91);
+      border-bottom: 1px solid rgba(47, 47, 47, 0.81);
+      text-align: center;
+      font-size: 25px;
+      font-family: 'Poiret One', cursive;
+  }
+  .listTasks{
+      grid-area: listTasks;
+      background: rgba(255, 255, 255, 0.91);
+      width: 100%;
+      padding: 0;
+      margin-left: 0px;
+  }
+  .listItem{
+      display: grid;
+      grid-template-columns: 10% 80% 10%;
+      grid-template-rows: auto;
+      grid-template-areas: "fa-1x taskDescription deleteTask";
+      list-style: none;
+      width: 100%;
+      margin-top: 15px;
+      text-align: center;
+      font-size: 30px;
+      font-family: 'Caveat', cursive;
+      border-bottom: 1px solid rgba(47, 47, 47, 0.81);
+  }
+  .fa-1x{
+      grid-area: fa-1x;
+      -webkit-transition: opacity .5s ease;
+      transition: opacity .5s ease;
+  }
+  .taskDescription{
+      grid-area: taskDescription;
+      text-align: center;
+  }
+  .deleteTask{
+      grid-area: deleteTask;
+      background: rgba(255, 255, 255, 0.91);
+      border: 1px solid rgba(255, 255, 255, 0.91);
+  }
+  .compliteTask{
+      text-decoration: line-through;
+      color: gray;
+  }
+  @media screen and (max-width:800px) {
+      .wrapper-main{
+        width: 80%;
+        margin-left: 10%;
+      }
+  }
+  @media screen and (max-width:500px) {
+      .wrapper-main{
+        width: 100%;
+        margin-left: 0%;
+      }
+      .listItem{
+        grid-template-columns: 10% 75% 15%;
+      }
+  }
 </style>
